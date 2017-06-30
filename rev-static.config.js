1281ff541a0075143d48c1876720bb690e0dac0a
york yao <yaoao12306@outlook.com>
2017-06-30 22:35:14 +0800

    lint config js file

--- rev-static.config.js
+++ rev-static.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    inputFiles: [
-        "demo/*.js",
-        "demo/*.css",
-        "demo/*.png",
-        "demo/*.ejs.html",
-    ],
-    excludeFiles: [
-        "demo/*-*.*",
-    ],
-    outputFiles: file => file.replace(".ejs", ""),
-    json: "demo/variables.json",
-    ejsOptions: {
-        rmWhitespace: true
-    },
-    sha: 256,
-    customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
-    noOutputFiles: [
-        "demo/worker.js",
-    ],
-    es6: "demo/variables.ts",
-    less: "demo/variables.less",
-    scss: "demo/variables.scss",
-};
+  inputFiles: [
+    'demo/*.js',
+    'demo/*.css',
+    'demo/*.png',
+    'demo/*.ejs.html'
+  ],
+  excludeFiles: [
+    'demo/*-*.*'
+  ],
+  outputFiles: file => file.replace('.ejs', ''),
+  json: 'demo/variables.json',
+  ejsOptions: {
+    rmWhitespace: true
+  },
+  sha: 256,
+  customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
+  noOutputFiles: [
+    'demo/worker.js'
+  ],
+  es6: 'demo/variables.ts',
+  less: 'demo/variables.less',
+  scss: 'demo/variables.scss'
+}
