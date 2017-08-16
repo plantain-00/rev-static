92cd453f77225d10c4862c5c5ab4af95f05107ef
york yao <yaoao12306@outlook.com>
2017-08-16 20:23:16 +0800

    remove namespace in types declaration

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,12 +1,10 @@
 declare module "lodash.flatten" {
     function flatten<T>(array: T[][]): T[];
     export = flatten;
-    namespace flatten { }
 }
 declare module "lodash.uniq" {
     function uniq<T>(array: T[]): T[];
     export = uniq;
-    namespace uniq { }
 }
 declare module "*.json" {
     export const version: string;
