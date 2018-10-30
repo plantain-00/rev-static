ce865a83e2b02f033918473ab94e696e7cbe4496
york yao <yaoao12306@outlook.com>
2018-10-30 21:01:29 +0800

    chore: add type-coverage check

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -18,7 +18,8 @@ module.exports = {
     js: `standard ${jsFiles}`,
     export: `no-unused-export ${tsFiles}`,
     commit: `commitlint --from=HEAD~1`,
-    markdown: `markdownlint README.md`
+    markdown: `markdownlint README.md`,
+    typeCoverage: 'type-coverage -p src --at-least 99'
   },
   test: [
     'tsc -p spec',
